518113969b6d9bf0c5f59cc9791b8ef5e44716a0
José Bolívar <blackencio33@gmail.com>
2018-10-22 06:21:36 -0400

    videollamada

--- app.js
+++ app.js
@@ -1,8 +1,12 @@
 const express = require('express');
+const nedb = require('nedb');
 
 const port = process.env.PORT || 8020;
 
 const app = express();
+
+// Database
+let db = new Datastore({ filename: './data.db', autoload: true });
 
 //Static
 app.use(express.static(__dirname + '/dist/'));
--- src/components/views/Tablero.vue
+++ src/components/views/Tablero.vue
@@ -1,34 +1,89 @@
 <template>
 <div class="container">
-     <div class="columns">
-          <div class="column is-9">
-               <section>
-                    <b-tabs position="is-centered" type="is-toggle-rounded">
-                         <b-tab-item label="Tabla de Dibujo" icon="palette" icon-pack="fas"></b-tab-item>
-                         <b-tab-item label="Editor de Texto" icon="edit" icon-pack="fas"></b-tab-item>
-                         <b-tab-item label="Editor de Codigo" icon="code" icon-pack="fas"></b-tab-item>
-                    </b-tabs>
-               </section>
-               <!--<a @click="connect()" class="button is-vcentered is-primary is-outlined is-rounded">Conectar</a>-->
+
+    <div class="columns" v-if="isClass">
+      <div class="column is-1">
+        <!--<p class="title is-6 has-text-centered">Menu</p>-->
+        <div class="card center">
+          <div class="card-content">
+            <a @click="isModalClassActive = true" class="button is-large is-vcentered is-success is-outlined ismenu">
+              <span class="icon is-large">
+                <i class="fas fa-plus-circle"></i>
+              </span>
+            </a>
+            <a class="button is-large is-vcentered is-danger is-outlined ismenu">
+              <span class="icon is-large">
+                <i class="fas fa-times-circle"></i>
+              </span>
+            </a>
           </div>
-          
-          <div class="column is-3">
-               <div class="card">
-                    <div class="card-content">
-                         <div id="subscribers" v-for="stream in streams" :key="stream.streamId" v-if="streams.length > 0">
-                              <subscriber @error="errorHandler" :opts="opts" :stream="stream" :session="session"></subscriber>
-                         </div>
-                         <div v-else>
-                              <p class="title is-6">Esperando...</p>
-                         </div>
-                    </div>
-                    <div class="card-content">
-                         <publisher :session="session" :opts="opts" @error="errorHandler"></publisher>
-                    </div>
-               </div>
-               <chat class="chat"></chat>
+        </div>
+
+      </div>
+
+      <div class="column is-11">
+
+        <div class="columns is-gapless is-multiline is-mobile">
+
+          <div class="column is-one-quarter">
+            <div class="card class">
+              <div class="card-header">
+                <p class="card-header-title">Clase de Matematicas</p>
+                <a class="card-header-icon" aria-label="more options">
+                  <b-icon :type="{ 'is-success': true }" icon="circle" pack="fas"></b-icon>
+                </a>
+              </div>
+              <div class="card-content">
+                <strong>Tema: </strong>Derivadas <br>
+                <strong>Profesor(a): </strong>José Daza <br>
+                <strong>Fecha inicio: </strong> 01/11/2018 <br>
+                <strong>Fecha fin: </strong> 02/11/2018
+              </div>
+              <div class="card-footer">
+                <a class="card-footer-item"><strong>Tomar esta clase..</strong></a>
+              </div>
+            </div>
           </div>
-     </div>
+
+        </div>
+
+      </div>
+
+      <b-modal :active.sync="isModalClassActive" has-modal-card>
+          <class></class>
+      </b-modal>
+
+    </div>
+
+    <div class="columns" v-else>
+        <div class="column is-9">
+              <section>
+                  <b-tabs position="is-centered" type="is-toggle-rounded">
+                        <b-tab-item label="Tabla de Dibujo" icon="palette" icon-pack="fas"></b-tab-item>
+                        <b-tab-item label="Editor de Texto" icon="edit" icon-pack="fas"></b-tab-item>
+                        <b-tab-item label="Editor de Codigo" icon="code" icon-pack="fas"></b-tab-item>
+                  </b-tabs>
+              </section>
+              <!--<a @click="connect()" class="button is-vcentered is-primary is-outlined is-rounded">Conectar</a>-->
+        </div>
+        
+        <div class="column is-3">
+              <div class="card">
+                  <div class="card-content">
+                        <div id="subscribers" v-for="stream in streams" :key="stream.streamId" v-if="streams.length > 0">
+                            <subscriber @error="errorHandler" :opts="opts" :stream="stream" :session="session"></subscriber>
+                        </div>
+                        <div v-else>
+                            <p class="title is-6">Esperando...</p>
+                        </div>
+                  </div>
+                  <div class="card-content">
+                        <publisher :session="session" :opts="opts" @error="errorHandler"></publisher>
+                  </div>
+              </div>
+              <chat class="chat"></chat>
+        </div>
+    </div>
 </div>
      
 </template>
@@ -38,6 +93,7 @@ import OT from "@opentok/client";
 import Publisher from "@/components/opentok/Publisher.vue";
 import Subscriber from "@/components/opentok/Subscriber.vue";
 import Chat from "@/components/views/Chat.vue";
+import Class from '@/components/views/Class.vue';
 
 const errorHandler = err => {
   alert(err.message);
@@ -60,10 +116,14 @@ export default {
         videoSource: true,
         height: "150px",
         width: "100%"
-      }
+      },
+
+      //Config Table
+      isClass: true,
+      isModalClassActive: false
     };
   },
-  components: { Publisher, Subscriber, Chat },
+  components: { Publisher, Subscriber, Chat, Class },
   created() {
     this.session = OT.initSession(this.apiKey, this.sessionId);
     this.session.connect(this.token, err => {
@@ -106,7 +166,14 @@ export default {
   align-items: center;
 }
 
+.ismenu{
+  margin-bottom: 0.5em;
+  margin-top: 0.3em;
+}
 
+.class{
+  margin: 0 0.5em 0.5em 0.5em;
+}
 
 </style>
 
