a39b88652361d9fad49e99e186215798557233ef
José Bolívar <blackencio33@gmail.com>
2018-11-01 06:56:37 -0400

    pusher chat

--- src/components/opentok/Subscriber.vue
+++ src/components/opentok/Subscriber.vue
@@ -4,7 +4,7 @@
 
 <script>
 export default {
-  name: 'subscriber',
+  name: "subscriber",
   props: {
     stream: {
       type: OT.Stream,
@@ -20,14 +20,19 @@ export default {
     }
   },
   mounted: function() {
-    const subscriber = this.session.subscribe(this.stream, this.$el, this.opts, (err) => {
-      if (err) {
-        this.$emit('error', err);
-      } else {
-        this.$emit('subscriberConnected', subscriber);
+    const subscriber = this.session.subscribe(
+      this.stream,
+      this.$el,
+      this.opts,
+      err => {
+        if (err) {
+          this.$emit("error", err);
+        } else {
+          this.$emit("subscriberConnected", subscriber);
+        }
       }
-    });
-    this.$emit('subscriberCreated', subscriber);
+    );
+    this.$emit("subscriberCreated", subscriber);
   }
 };
 </script>
--- src/components/views/Tablero.vue
+++ src/components/views/Tablero.vue
@@ -8,7 +8,7 @@
                            <vue-editor v-model="textEdit" id="editor"></vue-editor>
                          </b-tab-item>
                          <b-tab-item label="Editor de Codigo" icon="code" icon-pack="fas">
-                           <brace style="height: 500px" 
+                           <brace style="height: 400px" 
                               :fontsize="'12px'" 
                               :theme="'monokai'" 
                               :mode="'javascript'"
@@ -26,18 +26,42 @@
           
           <div class="column is-3">
                <div class="card">
-                    <div class="card-content">
-                         <div id="subscribers" v-for="stream in streams" :key="stream.streamId">
-                              <subscriber @error="errorHandler" :opts="optsTwo" :stream="stream" :session="session"></subscriber>
+                    <div class="card-content" ><!---show="streams.length > 0 ? true : false">-->
+                         <div id="subscribers" v-for="(stream, index) in streams" :key="index">
+                            <subscriber @error="errorHandler" :opts="optsTwo" :stream="stream" :session="session"></subscriber>
                          </div>
+                         <div id="subscriber"></div>
                     </div>
                     <div class="card-content center">
-                         <publisher :session="session" :opts="optsOne" @error="errorHandler"></publisher>
+                         <!--<publisher :session="session" :opts="optsOne" @error="errorHandler"></publisher>-->
+                         <div :id="'publisher'+index" v-for="(item, index) in statusPublisher" :key="index" v-show="item"></div>
                     </div>
                     <div class="card-footer">
-                      <a class="card-footer-item">Save</a>
-                      <a class="card-footer-item">Edit</a>
-                      <a class="card-footer-item">Delete</a>
+                      <a :class="
+                              isPublish == true ? 
+                              'button is-medium is-success is-outlined card-footer-item' : 
+                              'button is-medium is-danger is-outlined card-footer-item'"
+                              @click="showPublish()">
+                          <span class="icon is-medium">
+                            <i :class="isPublish == true ? 'fas fa-eye' : 'fas fa-eye-slash'"></i>
+                          </span>
+                      </a>
+                      <a :class="isPublishVideo == true ? 
+                                'button is-medium is-success is-outlined card-footer-item' : 
+                                'button is-medium is-danger card-footer-item'" 
+                                @click="isVideo()">
+                          <span class="icon is-medium">
+                          <i class="fas fa-video"></i>
+                          </span>
+                      </a>
+                      <a :class="isPublishAudio == true ? 
+                                'button is-medium is-success is-outlined card-footer-item' : 
+                                'button is-medium is-danger card-footer-item'" 
+                                @click="isAudio()">
+                          <span class="icon is-medium">
+                          <i class="fas fa-volume-up"></i>
+                          </span>
+                      </a>
                     </div>
                </div>
                <chat class="chat"></chat>
@@ -54,11 +78,10 @@ import Subscriber from "@/components/opentok/Subscriber.vue";
 import Chat from "@/components/views/Chat.vue";
 import { VueEditor } from "vue2-editor";
 
-import Brace from 'vue-bulma-brace'
-
+import Brace from "vue-bulma-brace";
 
 const errorHandler = err => {
-  alert(err.message);
+  console.log("error --> " + err);
 };
 
 export default {
@@ -72,13 +95,7 @@ export default {
         "T1==cGFydG5lcl9pZD00NjIwNDQwMiZzaWc9Mjk1NjIwYTIyZjM3MDBhMmI0NmU2M2Q2MDk4MjNjMmM5ZjVhZWU5NjpzZXNzaW9uX2lkPTJfTVg0ME5qSXdORFF3TW41LU1UVTBNRGs0TnpNME56VTVPWDV4YWtGUWVFSlJVRzgxUVZaVlRqRTBjbU5uWmt0NVdtNS1RWDQmY3JlYXRlX3RpbWU9MTU0MDk4NzM0NyZyb2xlPW1vZGVyYXRvciZub25jZT0xNTQwOTg3MzQ3LjYzOTMxMTE2MTQyMTY0JmV4cGlyZV90aW1lPTE1NDE1OTIxNDcmY29ubmVjdGlvbl9kYXRhPW5hbWUlM0RKb2hubnk=",
       streams: [],
       session: null,
-      optsTwo: {
-        publishAudio: true,
-        publishVideo: true,
-        videoSource: true,
-        height: "150px",
-        width: "100%"
-      },
+
       optsOne: {
         publishAudio: true,
         publishVideo: true,
@@ -88,20 +105,59 @@ export default {
       },
 
       //Editor de Texto
-      textEdit: '',
+      textEdit: "",
 
       //Editor de Codigo
-      code: 'const noop = () => {}'
+      code: "const noop = () => {}",
+
+      //Publish
+      isPublish: false,
+      publisher: null,
+      statusPublisher: [
+        false,
+        false,
+        false,
+        false,
+        false,
+        false,
+        false,
+        false,
+        false,
+        false
+      ],
+      contPublisher: 0,
+      optsTwo: {
+        publishAudio: true,
+        publishVideo: true,
+        videoSource: true,
+        height: "150px",
+        width: "100%"
+      },
+      isPublishVideo: true,
+      isPublishAudio: true
     };
   },
-  components: { Publisher, Subscriber, Chat, VueEditor, Brace},
+  components: { Chat, VueEditor, Brace, Subscriber },
   created() {
-   /* this.session = OT.initSession(this.apiKey, this.sessionId);
-    this.session.connect(this.token, err => {
-      if (err) {
-        errorHandler(err);
+    this.session = OT.initSession(this.apiKey, this.sessionId);
+    this.session.connect(
+      this.token,
+      err => {
+        if (err) {
+          errorHandler(err);
+        }
       }
-    });
+    );
+
+    if (this.publisher != null) {
+      this.publisher.on("streamCreated", function(event) {
+        console.log("The publisher started streaming.");
+      });
+      this.publisher.on("streamDestroyed", function(event) {
+        console.log("The publisher stopped streaming. Reason: " + event.reason);
+      });
+    }
+
     this.session.on("streamCreated", event => {
       this.streams.push(event.stream);
     });
@@ -110,19 +166,62 @@ export default {
       if (idx > -1) {
         this.streams.splice(idx, 1);
       }
-    });*/
+    });
   },
   methods: {
     errorHandler,
 
-    connect() {
-      this.videoSource = true;
-      this.publishVideo = true;
+    isVideo() {
+      if (this.isPublishVideo == true) {
+        this.publisher.publishVideo(false);
+        this.isPublishVideo = false
+      } else {
+        this.publisher.publishVideo(true);
+        this.isPublishVideo = true
+      }
       this.$forceUpdate();
     },
 
-    //Editor de Codigo
+    isAudio() {
+      if (this.isPublishAudio == true) {
+        this.publisher.publishAudio(false);
+        this.isPublishAudio = false
+      } else {
+        this.publisher.publishAudio(true);
+        this.isPublishAudio = true
+      }
+      this.$forceUpdate();
+    },
 
+    showPublish() {
+      if (this.isPublish == false) {
+        this.statusPublisher[this.contPublisher] = true;
+
+        this.publisher = OT.initPublisher(
+          `publisher${this.contPublisher}`,
+          this.optsOne,
+          err => {
+            if (err) console.log(err);
+          }
+        );
+        this.session.publish(this.publisher, err => {
+          if (err) {
+            console.log("error", err);
+          }
+        });
+
+        this.isPublish = true;
+        this.contPublisher += 1;
+      } else {
+        this.session.unpublish(this.publisher);
+        this.publisher = null;
+        this.isPublish = false;
+        this.isPublishVideo = true
+        this.isPublishAudio = true
+        this.statusPublisher[this.contPublisher - 1] = true;
+      }
+      this.$forceUpdate();
+    }
   }
 };
 </script>
@@ -139,13 +238,12 @@ export default {
   justify-content: center;
   align-items: center;
 }
-
 </style>
 
 <style>
 .ql-editor {
-    min-height: 500px;
-    max-height: 70vh;
+  min-height: 500px;
+  max-height: 70vh;
 }
 </style>
 
